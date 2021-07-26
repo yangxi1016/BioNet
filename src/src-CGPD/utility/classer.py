b8a79b8a1eb2c2ff51aeec068fd077436489739c
yangxi1016 <yangxi1016@nudt.edu.cn>
2021-07-26 16:05:14 +0800

    Add files via upload

---
+++ src/src-CGPD/utility/classer.py
@@ -0,0 +1,137 @@
+import logging
+import torch
+from torch import nn
+import math
+import numpy as np
+from typing import Optional, Iterator
+from torch.utils.data import Sampler, Dataset
+import torch.distributed as dist
+from collections import defaultdict
+from utility.function import mkdir
+
+
+""" BucketSampler """
+class BucketSampler(Sampler):
+    def __init__(self,
+                 dataset: Dataset,
+                 rc_lens_list,
+                 batch_size,
+                 is_dist: bool = True,
+                 num_replicas: Optional[int] = None,
+                 rank: Optional[int] = None) -> None:
+        if is_dist:
+            if num_replicas is None:
+                if not dist.is_available():
+                    raise RuntimeError("Requires distributed package to be available")
+                num_replicas = dist.get_world_size()
+            if rank is None:
+                if not dist.is_available():
+                    raise RuntimeError("Requires distributed package to be available")
+                rank = dist.get_rank()
+            if rank >= num_replicas or rank < 0:
+                raise ValueError(
+                    "Invalid rank {}, rank should be in the interval"
+                    " [0, {}]".format(rank, num_replicas - 1))
+        else:
+            num_replicas = 1
+            rank = 0
+
+        self.num_samples = 0
+        self.dataset = dataset
+        self.rc_lens_list = rc_lens_list
+        self.batch_size = batch_size
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+
+    def __iter__(self) -> Iterator:
+
+        '''
+        separate indices into <num_replicas> ranks
+        '''
+        indices_all = list(range(len(self.dataset)))  # all relation category train edges index
+
+        n_rc = len(self.rc_lens_list)  # all relation category count
+        rc_idx_list = list(range(n_rc))  # relation category index [0, 1, 2, 3, ...]
+        rc_btn_list = [0]*n_rc  # relation category batch num [0, 0, 0, 0, ...]
+
+        i = 0
+        indices_dict = defaultdict(list)
+        while True:
+            ### chose relation category randomly
+            cu_rc_idx = np.random.choice(rc_idx_list)  # current relation category index
+
+            rc_s = sum(self.rc_lens_list[:cu_rc_idx])  # relation category train edges start index
+            rc_e = rc_s + self.rc_lens_list[cu_rc_idx]  # relation category train edges end index
+            rc_indices = indices_all[rc_s:rc_e]
+
+            bt_s = rc_btn_list[cu_rc_idx] * self.batch_size  # relation category batch index start
+            bt_e = bt_s + self.batch_size  # relation category batch index end
+            bt_indices = rc_indices[bt_s:bt_e]
+            rc_btn_list[cu_rc_idx] += 1  # update relation category batch num
+
+            ### rank id
+            rank = i % self.num_replicas
+            ### add indices to the current rank
+            indices_dict[rank] += bt_indices
+            ### rank id ++
+            i += 1
+
+            ### if current relation category is not enough for next batch, remove it
+            if len(rc_indices) - bt_e < self.batch_size:
+                rc_idx_list.remove(cu_rc_idx)
+
+            if len(rc_idx_list) == 0:
+                break
+
+        ### subsample for each rank
+        indices = indices_dict[self.rank]
+        self.num_samples = len(indices)
+
+        return iter(indices)
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def set_epoch(self, epoch: int) -> None:
+        r"""
+        Sets the epoch for this sampler. When :attr:`shuffle=True`, this ensures all replicas
+        use a different random ordering for each epoch. Otherwise, the next iteration of this
+        sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = epoch
+
+
+""" HingeLoss """
+class HingeLoss(nn.Module):
+    def __init__(self, margin):
+        super(HingeLoss, self).__init__()
+
+        self.margin = margin
+
+    def forward(self, pos, neg):
+        diff = torch.relu(torch.sub(neg, pos - self.margin))
+        loss = torch.sum(diff)
+        return loss
+
+
+""" logger """
+def logger(log_file):
+    logging.basicConfig(
+                    level=logging.DEBUG,
+                    format="%(asctime)s - %(name)s : %(levelname)s - %(message)s",
+                    datefmt="%y-%m-%d %H:%M:%S",
+                    filename=log_file,
+                    filemode="w")
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(name)s : %(levelname)s - %(message)s")
+    console.setFormatter(formatter)
+
+    logging.getLogger("").addHandler(console)
+    # logging.warning("Jackdaws love my big sphinx of quartz.")
+
+    return logging
---
+++ src/src-CGPD/utility/prepare.py
@@ -0,0 +1,273 @@
+import os
+import pickle
+import numpy as np
+import scipy.sparse as sp
+import torch
+from torch.utils.data import TensorDataset
+
+
+class DataPreparation:
+
+    def __init__(self, args):
+
+        self.args = args
+
+        self.is_train = True
+        self.dropout = 0.
+
+        '''
+        load node dict
+        '''
+        ### load n_chems & n_genes & n_paths
+        self.cid2num_dict = pickle.load(open(args.adj_pkl_path + "cid2num_dict.pkl", "rb"))
+        self.gid2num_dict = pickle.load(open(args.adj_pkl_path + "gid2num_dict.pkl", "rb"))
+        self.pid2num_dict = pickle.load(open(args.adj_pkl_path + "pid2num_dict.pkl", "rb"))
+        self.did2num_dict = pickle.load(open(args.adj_pkl_path + "did2num_dict.pkl", "rb"))
+        self.n_chems = len(self.cid2num_dict.keys())
+        self.n_genes = len(self.gid2num_dict.keys())
+        self.n_paths = len(self.pid2num_dict.keys())
+        self.n_diss = len(self.did2num_dict.keys())
+
+        '''
+        load adj
+        '''
+        ### chem1-chem2; chem2-chem1; gene1-gene2; gene2-gene1
+        self.chem1_chem2_adj = pickle.load(open(args.adj_pkl_path + "chem1_chem2_adj.pkl", "rb"))
+        self.gene1_gene2_adj = pickle.load(open(args.adj_pkl_path + "gene1_gene2_adj.pkl", "rb"))
+        ### chem-path; path-chem; gene-path; path-gene
+        self.chem_path_adj = pickle.load(open(args.adj_pkl_path + "chem_path_adj.pkl", "rb"))
+        self.gene_path_adj = pickle.load(open(args.adj_pkl_path + "gene_path_adj.pkl", "rb"))
+        self.path_chem_adj = pickle.load(open(args.adj_pkl_path + "path_chem_adj.pkl", "rb"))
+        self.path_gene_adj = pickle.load(open(args.adj_pkl_path + "path_gene_adj.pkl", "rb"))
+        #dis
+        self.chem_dis_adj = pickle.load(open(args.adj_pkl_path + "chem_dis_adj.pkl", "rb"))
+        self.gene_dis_adj = pickle.load(open(args.adj_pkl_path + "gene_dis_adj.pkl", "rb"))
+        self.dis_chem_adj = pickle.load(open(args.adj_pkl_path + "dis_chem_adj.pkl", "rb"))
+        self.dis_gene_adj = pickle.load(open(args.adj_pkl_path + "dis_gene_adj.pkl", "rb"))
+        ### chem-gene; gene-chem
+        self.chem_gene_adj_list = pickle.load(open(args.adj_pkl_path + "chem_gene_adj_list.pkl", "rb"))
+        self.gene_chem_adj_list = pickle.load(open(args.adj_pkl_path + "gene_chem_adj_list.pkl", "rb"))
+
+        '''
+        load deg
+        '''
+        ### chem1-chem2; chem2-chem1; gene1-gene2; gene2-gene1
+        self.chem1_chem2_deg = pickle.load(open(args.adj_pkl_path + "chem1_chem2_deg.pkl", "rb"))
+        self.gene1_gene2_deg = pickle.load(open(args.adj_pkl_path + "gene1_gene2_deg.pkl", "rb"))
+        ### chem-path; path-chem; gene-path; path-gene
+        self.chem_path_deg = pickle.load(open(args.adj_pkl_path + "chem_path_deg.pkl", "rb"))
+        self.gene_path_deg = pickle.load(open(args.adj_pkl_path + "gene_path_deg.pkl", "rb"))
+        self.path_chem_deg = pickle.load(open(args.adj_pkl_path + "path_chem_deg.pkl", "rb"))
+        self.path_gene_deg = pickle.load(open(args.adj_pkl_path + "path_gene_deg.pkl", "rb"))
+        #dis
+        self.chem_dis_deg = pickle.load(open(args.adj_pkl_path + "chem_dis_deg.pkl", "rb"))
+        self.gene_dis_deg = pickle.load(open(args.adj_pkl_path + "gene_dis_deg.pkl", "rb"))
+        self.dis_chem_deg = pickle.load(open(args.adj_pkl_path + "dis_chem_deg.pkl", "rb"))
+        self.dis_gene_deg = pickle.load(open(args.adj_pkl_path + "dis_gene_deg.pkl", "rb"))
+        # chem-gene; gene-chem
+        self.chem_gene_deg_list = pickle.load(open(args.adj_pkl_path + "chem_gene_deg_list.pkl", "rb"))
+        self.gene_chem_deg_list = pickle.load(open(args.adj_pkl_path + "gene_chem_deg_list.pkl", "rb"))
+
+        '''
+        set adj, deg
+        '''
+        ### relation type adjacency dictionary; s1: subgraph 1, s2: subgraph 2
+        self.adj_dict_s1 = {}
+        self.adj_dict_s2 = {}
+        self.set_adj_dict()
+
+        ### train edges => relation type adjacency dictionary; s1: subgraph 1, s2: subgraph 2
+        self.adj_dict_s1_tr = {}
+        self.adj_dict_s2_tr = {}
+        self.set_adj_dict_tr(args)
+
+        ### relation type num dictionary
+        self.rt_num_dict_s1 = {}
+        self.rt_num_dict_s2 = {}
+        self.set_rt_num_dict()
+
+        ### relation type dimension dictionary
+        self.rt_dim_dict_s1 = {}
+        self.rt_dim_dict_s2 = {}
+        self.set_rt_dim_dict()
+
+        ### relation category to index dictionary
+        self.rc2idx_dict_s1 = {}
+        self.rc2idx_dict_s2 = {}
+        self.idx2rc_dict_s1 = {}
+        self.idx2rc_dict_s2 = {}
+        self.set_rc_idx_dict()
+
+        ### relation type degree dictionary
+        self.deg_dict_s1 = {}
+        self.deg_dict_s2 = {}
+        self.set_deg_dict()
+
+        '''
+        set feat
+        '''
+        self.feat_dict = {}
+        self.feat_num_dict = {}
+        self.nonzero_feat_num_dict = {}
+        self.set_feat_dict()
+
+        '''
+        set adj dimension
+        '''
+        self.adj_dim_dict_s1 = {}
+        self.adj_dim_dict_s2 = {}
+        self.set_adj_dim_dict()
+
+
+    ### set_adj_dict
+    def set_adj_dict(self):
+        '''
+        relation type definition
+        '''
+        # relation type adjacency dictionary; s1: subgraph 1, s2: subgraph 2
+        self.adj_dict_s1 = {
+            (0, 0): [self.chem1_chem2_adj],
+            (0, 2): [self.chem_path_adj],
+            (1, 1): [self.gene1_gene2_adj],
+            (1, 2): [self.gene_path_adj],
+            (2, 0): [self.path_chem_adj],
+            (2, 1): [self.path_gene_adj],
+            (1, 3): [self.gene_dis_adj],
+            (3, 0): [self.dis_chem_adj],
+            (3, 1): [self.dis_gene_adj],
+            (0, 3): [self.chem_path_adj],
+        }
+        self.adj_dict_s2 = {
+            (0, 1): self.chem_gene_adj_list,
+            (1, 0): self.gene_chem_adj_list,
+        }
+
+
+    ### set_adj_dict_tr
+    def set_adj_dict_tr(self, args):
+        path1 = args.edg_pkl_path + "adj_dict_s1_tr.pkl"
+        path2 = args.edg_pkl_path + "adj_dict_s2_tr.pkl"
+        if os.path.exists(path1) and os.path.exists(path2):
+            self.adj_dict_s1_tr = pickle.load(open(path1, "rb"))
+            self.adj_dict_s2_tr = pickle.load(open(path2, "rb"))
+
+
+    ### set rt_num_dict
+    def set_rt_num_dict(self):
+        # relation type num dictionary
+        self.rt_num_dict_s1 = {k: len(v) for k, v in self.adj_dict_s1.items()}
+        self.rt_num_dict_s2 = {k: len(v) for k, v in self.adj_dict_s2.items()}
+
+
+    ### set_rt_dim_dict
+    def set_rt_dim_dict(self):
+        # relation type dimension dictionary
+        self.rt_dim_dict_s1 = {k: [adj.shape for adj in adjs] for k, adjs in self.adj_dict_s1.items()}
+        self.rt_dim_dict_s2 = {k: [adj.shape for adj in adjs] for k, adjs in self.adj_dict_s2.items()}
+
+
+    ### set_rc_idx_dict
+    def set_rc_idx_dict(self):
+        idx = 0
+        for i, j in self.rt_num_dict_s1:
+            for k in range(self.rt_num_dict_s1[(i, j)]):
+                self.rc2idx_dict_s1[(i, j, k)] = idx
+                self.idx2rc_dict_s1[idx] = (i, j, k)
+                idx = idx + 1
+
+        idx = 0
+        for i, j in self.rt_num_dict_s2:
+            for k in range(self.rt_num_dict_s2[(i, j)]):
+                self.rc2idx_dict_s2[(i, j, k)] = idx
+                self.idx2rc_dict_s2[idx] = (i, j, k)
+                idx = idx + 1
+
+
+    ### set_deg_dict
+    def set_deg_dict(self):
+        self.deg_dict_s1 = {
+            (0, 0): [self.chem1_chem2_deg],
+            (0, 2): [self.chem_path_deg],
+            (1, 1): [self.gene1_gene2_deg],
+            (1, 2): [self.gene_path_deg],
+            (2, 0): [self.path_chem_deg],
+            (2, 1): [self.path_gene_deg],
+            (1, 3): [self.gene_dis_deg],
+            (3, 0): [self.dis_chem_deg],
+            (3, 1): [self.dis_gene_deg],
+            (0, 3): [self.chem_dis_deg],
+        }
+        self.deg_dict_s2 = {
+            (0, 1): self.chem_gene_deg_list,
+            (1, 0): self.gene_chem_deg_list,
+        }
+
+
+    ### set_feat_dict
+    def set_feat_dict(self):
+        # feature (chem & gene & path)
+        chem_feat = sp.identity(self.n_chems, dtype=float)
+        chem_nonzero_feat_num, chem_feat_num = chem_feat.shape
+        gene_feat = sp.identity(self.n_genes, dtype=float)
+        gene_nonzero_feat_num, gene_feat_num = gene_feat.shape
+        path_feat = sp.identity(self.n_paths, dtype=float)
+        path_nonzero_feat_num, path_feat_num = path_feat.shape
+        dis_feat = sp.identity(self.n_diss, dtype=float)
+        dis_nonzero_feat_num, dis_feat_num = dis_feat.shape
+        self.feat_dict = {
+            0: chem_feat,
+            1: gene_feat,
+            2: path_feat,
+            3: dis_feat,
+        }
+        self.feat_num_dict = {
+            0: chem_feat_num,
+            1: gene_feat_num,
+            2: path_feat_num,
+            3: dis_feat_num,
+        }
+        self.nonzero_feat_num_dict = {
+            0: chem_nonzero_feat_num,
+            1: gene_nonzero_feat_num,
+            2: path_nonzero_feat_num,
+            3: dis_nonzero_feat_num,
+        }
+
+
+    ### set_adj_dim_dict
+    def set_adj_dim_dict(self):
+        self.adj_dim_dict_s1 = {k: [adj.shape for adj in adjs] for k, adjs in self.adj_dict_s1.items()}
+        self.adj_dim_dict_s2 = {k: [adj.shape for adj in adjs] for k, adjs in self.adj_dict_s2.items()}
+
+
+    ### set dropout
+    def set_dropout(self, dropout):
+        self.dropout = dropout
+
+
+    ### load edges
+    def load_edges(self, file):
+        ### load train edges
+        edges = pickle.load(open(self.args.edg_pkl_path + file, "rb"))
+        rc_idx = 0  # relation category index
+        rc_lens_list = []
+        edges_list = []
+        labels_list = []
+        for rel_type, edge_lists in edges.items():
+            for edge_list in edge_lists:
+                rc_lens_list.append(len(edge_list))
+                ### shuffle
+                np.random.shuffle(edge_list)
+                for edge in edge_list:
+                    edges_list.append(edge)
+                    labels_list.append(rc_idx)
+                rc_idx = rc_idx + 1
+        edges_array = np.array(edges_list)
+        labels_array = np.array(labels_list)
+        edges = TensorDataset(torch.from_numpy(edges_array), torch.from_numpy(labels_array))
+        return edges, rc_lens_list
+
+
+
+
+
